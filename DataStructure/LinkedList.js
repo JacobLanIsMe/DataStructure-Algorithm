a8b30a790fcc18fe262b1d972c144e9215912c14
Jacob <maimaisatt@gmail.com>
2025-01-01 22:09:43 +0800

    add mark

--- DataStructure/LinkedList.js
+++ DataStructure/LinkedList.js
@@ -1,3 +1,32 @@
+// Advantages:
+// 1. Elements can be inserted into linked lists indefinitely, while an array will eventually either fill up or need to be resized.
+// 2. Very fast insertion and deletion compared to Array.
+
+// Disadvantages:
+// 1. They use more memory than arrays because of the storage used by their pointers.
+// 2. Nodes in a linked list must be read in order from the beginning as linked lists are inherently sequential access.
+// 3. Nodes are stored noncontiquous, greatly increasing the time periods required to access individual elements within the list, especially with a CPU cache.
+// 4. Difficulties arise in linked lists when it comes to reverse traversing. For instance, single-linked lists are cumbersome to navigate backward and while doubly linked lists are somewhat easier to read, memory is consumed in allocating space for a back-pointer.
+
+// Difference between Array and Linked List
+// Linked List:
+// 1. Do not have indices.
+// 2. Connection between nodes are a "next" pointer.
+// 3. Random access is not allowed (Must go throught each item before accessing). => 無法直接取得某個 index 的值。
+// Array:
+// Items are indexed in order.
+// Insertion and deletion are expensive.
+// Elements can quickly be accessed with a specific index.
+
+// Big O of Array and Linked List
+//                                           Array     Linked List
+// Accessing Elements                         O(1)        O(n)
+// Insert and Remove from the Beginning       O(n)        O(1)
+// Insert and Remove from the End             O(1)        O(n)
+// Insert and Remove from the Middle          O(n)        O(n)
+
+
+
 class Node {
     constructor(value) {
         this.value = value;
